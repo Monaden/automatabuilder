389ce7a78d1a39ffcf90cb67fa6975a2e2c56734
Oliv <axel.olivecrona1995@gmail.com>
2015-05-14 23:53:54 +0200

    implementet interator for Word

--- src/main/java/automatabuilder/Word.java
+++ src/main/java/automatabuilder/Word.java
@@ -1,14 +1,42 @@
 package automatabuilder;
 
 import interfaces.IWord;
+import java.lang.Iterable;
+import java.util.Iterator;
 
-public class Word implements IWord {
+public class Word implements IWord, Iterable<Symbol> {
 
     private Symbol head;
     private IWord tail;
 
     //TODO not sure how to implement
     public Word () {
+    }
+
+    @Override
+    public Iterator<Symbol> iterator() {
+        return new Iterator<Symbol>() {
+            Symbol currentHead = head;
+            IWord currentTail = tail;
+
+            @Override
+            public boolean hasNext() {
+                return !head.equals("");
+            }
+
+            @Override
+            public Symbol next() {
+                Symbol next = currentHead;
+                currentHead = currentTail.head();
+                currentTail = currentTail.tail();
+                return next;
+            }
+
+            @Override
+            public void remove(){
+                throw new UnsupportedOperationException("remove not supported for Word");
+            }
+        };
     }
 
     public String toString() {
