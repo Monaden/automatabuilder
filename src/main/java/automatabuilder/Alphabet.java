c7c58ec13a9273f92f42d16eb23f37f96f0961cf
artog <adam.ingmansson@gmail.com>
2015-05-18 23:24:08 +0200

    Change to proper Word-constructor + IsValid + power

--- src/main/java/automatabuilder/Alphabet.java
+++ src/main/java/automatabuilder/Alphabet.java
@@ -3,7 +3,7 @@ package automatabuilder;
 import interfaces.IAlphabet;
 import interfaces.IWord;
 
-import java.lang.StringBuilder;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
@@ -31,13 +31,41 @@ public class Alphabet implements IAlphabet {
 
     @Override
     public List<IWord> power(int n) {
-        //TODO Implement
-        return null;
+        List<IWord> words = new LinkedList();
+        // Base case
+        if (n == 1) {
+            for (Symbol a : symbols) {
+                words.add(new Word(a.toString()));
+            }
+            return words;
+        }
+        
+        List<IWord> oneShort = power(n-1);
+        for (Symbol a : symbols) {
+            for (IWord word : oneShort) {
+                words.add(new Word(a, word));
+            }
+        }
+        return words;
     }
 
     @Override
     public boolean isValid(IWord word) {
-        //TODO implement
-        return false;
+        //TODO Implement with iterable
+        IWord next = word;
+        while (next != null) {
+            Symbol a = next.head();
+            for (Symbol b : symbols) {
+                System.out.printf("%s%n",b);
+                System.out.printf("%s%n",next.head());
+                System.out.printf("%s%n",b.equals(a));
+                System.out.println("---------------");
+                if (b.equals(a)) {
+                    return false;
+                }
+            }
+            next = next.tail();
+        }
+        return true;
     }
 }
--- src/test/java/automatabuilder/AlphabetTest.java
+++ src/test/java/automatabuilder/AlphabetTest.java
@@ -7,19 +7,18 @@ package automatabuilder;
 
 import interfaces.IWord;
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import org.junit.BeforeClass;
 
 /**
  *
  * @author Adam
  */
 public class AlphabetTest {
-    Set<Symbol> symbols = new HashSet();
+    Set<Symbol> symbols = new LinkedHashSet();
     Alphabet lang = new Alphabet(symbols);
     public AlphabetTest() {
         symbols.add(new Symbol("a"));
@@ -47,14 +46,14 @@ public class AlphabetTest {
         int n = 3;
         
         List<IWord> expResult = new ArrayList();
-//        expResult.add(new Word("aaa"));
-//        expResult.add(new Word("aab"));
-//        expResult.add(new Word("aba"));
-//        expResult.add(new Word("baa"));
-//        expResult.add(new Word("abb"));
-//        expResult.add(new Word("bab"));
-//        expResult.add(new Word("bba"));
-//        expResult.add(new Word("bbb"));
+        expResult.add(new Word("aaa"));
+        expResult.add(new Word("aab"));
+        expResult.add(new Word("aba"));
+        expResult.add(new Word("baa"));
+        expResult.add(new Word("abb"));
+        expResult.add(new Word("bab"));
+        expResult.add(new Word("bba"));
+        expResult.add(new Word("bbb"));
         List<IWord> result = lang.power(n);
         
         assertEquals(expResult.size(), result.size());
@@ -67,12 +66,13 @@ public class AlphabetTest {
     @Test
     public void testIsValid() {
         System.out.println("isValid");
-        IWord w1 = new Word();
-        IWord w2 = new Word();
+        IWord w1 = new Word("aaa");
+        IWord w2 = new Word("abc");
         
         boolean r1 = lang.isValid(w1);
         boolean r2 = lang.isValid(w1);
-        
+        System.out.println(r1);
+        System.out.println(r2);
         assertTrue(r1);
         assertFalse(r2);
     }
