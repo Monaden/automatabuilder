99e1374e483c0bf1dd1275b939c99934476f3334
Oliv <axel.olivecrona1995@gmail.com>
2015-05-20 19:41:10 +0200

    added files for strategy pattern

---
+++ src/main/java/automatabuilder/TableToConsole.java
@@ -0,0 +1,12 @@
+package automatabuilder;
+
+import interfaces.IShowTransitionTable;
+
+/**
+ * Created by oliv on 5/20/15.
+ */
+public class TableToConsole implements IShowTransitionTable {
+
+    public void showTable(){
+    }
+}
---
+++ src/main/java/interfaces/IShowTransitionTable.java
@@ -0,0 +1,8 @@
+package interfaces;
+
+/**
+ * Created by oliv on 5/20/15.
+ */
+public interface IShowTransitionTable {
+    public void showTable();
+}
