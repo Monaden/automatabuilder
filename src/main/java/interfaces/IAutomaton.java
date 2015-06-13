f2b7b26718658bceb17bdec6c4889afcd7049634
artog <adam.ingmansson@gmail.com>
2015-06-13 14:46:18 +0200

    Added accessable print table through IAutomaton

--- src/main/java/interfaces/IAutomaton.java
+++ src/main/java/interfaces/IAutomaton.java
@@ -1,7 +1,9 @@
 package interfaces;
 
 import exceptions.InvalidWordException;
+import java.io.PrintStream;
 
 public interface IAutomaton {
   public boolean test(String word) throws InvalidWordException;
+  public void printTo(PrintStream ps);
 }
